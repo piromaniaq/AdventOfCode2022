d930750e794d9c7e9d6cde0635a888b669ca8cdd
Oskar Litka <piromaniaq@gmail.com>
2022-12-07 21:36:05 +0100

    day2 part2

--- Day2/day2part2.py
+++ Day2/day2part2.py
@@ -6,9 +6,9 @@ def get_data(file):
 
 def total(rounds):
     outcomes = {
-        "A X":4, "A Y":8, "A Z":3,
+        "A X":3, "A Y":4, "A Z":8,
         "B X":1, "B Y":5, "B Z":9,
-        "C X":7, "C Y":2, "C Z":6
+        "C X":2, "C Y":6, "C Z":7
     }
     points = 0
     for round in rounds:
@@ -17,17 +17,17 @@ def total(rounds):
 
 
 if __name__ == '__main__':
-    input = get_data('inputtest.txt')
+    input = get_data('input.txt')
     answer = total(input)
     print(answer)
 
     #OUTCOMES
-    # A vs X = DRAW = 1 + 3 = 4
-    # A vs Y = WIN  = 2 + 6 = 8
-    # A vs Z = LOSE = 3 + 0 = 3
+    # A vs X = LOSE = 3 + 0 = 3
+    # A vs Y = DRAW = 1 + 3 = 4
+    # A vs Z = WIN  = 2 + 6 = 8
     # B vs X = LOSE = 1 + 0 = 1
     # B vs Y = DRAW = 2 + 3 = 5
     # B vs Z = WIN  = 3 + 6 = 9
-    # C vs X = WIN  = 1 + 6 = 7
-    # C vs Y = LOSE = 2 + 0 = 2
-    # C vs Z = DRAW = 3 + 3 = 6
+    # C vs X = LOSE = 2 + 0 = 2
+    # C vs Y = DRAW = 3 + 3 = 6
+    # C vs Z = WIN  = 1 + 6 = 7
